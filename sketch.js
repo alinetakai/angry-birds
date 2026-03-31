6fc4ac0ebdb0ec3ae5ec70f46c2d74a49ce7512c
Aline Takai <alinearaujothomaz@gmail.com>
2026-03-31 19:02:36 -0300

    first commit

---
+++ sketch.js
@@ -0,0 +1,48 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+let engine;
+let world;
+let canvas;
+let backgroundImg;
+let ground;
+let platform;
+let box1, box2;
+let pig1;
+let log1;
+
+function preload() {
+    backgroundImg = loadImage("./sprites/bg.png");
+}
+
+function setup() {
+    canvas  = createCanvas(1200, 400);
+    canvas.position(15, 70);
+
+    engine = Engine.create();
+    world = engine.world;
+
+    ground = new Ground(600, height, 1200, 20);
+    platform = new Ground(150, 305, 300, 170);
+
+    box1 = new Box(700, 320, 70, 70);
+    box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810, 350);
+    log1 = new Log(810, 260, 300, PI/2);
+}
+
+function draw() {
+    background(backgroundImg);
+
+    ground.display();
+    platform.display();
+
+    box1.display();
+    box2.display();
+    pig1.display();
+    pig1.score();
+    log1.display();
+
+}
